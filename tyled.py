7c8f4f533c1fc0f0fc56400e9726a9354756b158
Harvey Hunt <harveyhuntnexus@gmail.com>
2014-07-17 14:42:34 +0100

    Made some arguments required as well adding filter support.

--- tyled.py
+++ tyled.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 import argparse
-from PIL import Image
+from PIL import Image, ImageFilter
 
 class ImageNotSquare(Exception):
     pass
@@ -28,21 +28,48 @@ def main(args):
                 print('Placing tile at ({0}, {1})'.format(x, y))
             out.paste(tile, (x, y))
 
+    if args.filters:
+        if args.verbose:
+            print("Applying filters {0}".format(args.filters))
+        out = apply_filters(out, args.filters)
+
     out.save(args.out)
 
     if args.show:
         out.show()
 
+def apply_filters(img, filters):
+
+    filter_funcs = {'blur': ImageFilter.BLUR,
+            'contour': ImageFilter.CONTOUR,
+            'detail': ImageFilter.DETAIL,
+            'edge_enhance': ImageFilter.EDGE_ENHANCE,
+            'edge_enhance_more': ImageFilter.EDGE_ENHANCE_MORE,
+            'emboss': ImageFilter.EMBOSS,
+            'find_edges': ImageFilter.FIND_EDGES,
+            'smooth': ImageFilter.SMOOTH,
+            'smooth_more': ImageFilter.SMOOTH_MORE,
+            'sharpen': ImageFilter.SHARPEN}
+
+    for filter in filters:
+        img = img.filter(filter_funcs[filter])
+
+    return img
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description=
                                 'A lightweight image tiler written in Python.',
                                 conflict_handler='resolve')
-    parser.add_argument('-t', '--tile', type=str, help='The image to be tiled')
-    parser.add_argument('-o', '--out', type=str, help='The name of the image used as output')
+    parser.add_argument('-t', '--tile', type=str, help='The image to be tiled',
+            required=True)
+    parser.add_argument('-o', '--out', type=str, help='The name of the image used as output',
+            required=True)
     parser.add_argument('-bg', '--background', type=str, default='#000000',
             help='The background colour that will be displayed where the tile has alpha')
-    parser.add_argument('-w', '--width', type=int)
-    parser.add_argument('-h', '--height', type=int)
+    parser.add_argument('-w', '--width', type=int, required=True)
+    parser.add_argument('-h', '--height', type=int, required=True)
+    parser.add_argument('-f', '--filters', action='append')
     parser.add_argument('-s', '--show', action='store_true',
             help='Show the image upon completion')
     parser.add_argument('-v', '--verbose', action='store_true')
